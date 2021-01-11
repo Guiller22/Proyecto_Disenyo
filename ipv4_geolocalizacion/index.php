2a6ade0d843e547fcab87982639ccdb106ac0b51
guille <miito2010v2@hotmail.com>
2021-01-11 11:39:06 +0100

    s

--- ipv4_geolocalizacion/index.php
+++ ipv4_geolocalizacion/index.php
@@ -1,4 +1,22 @@
 <?php
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+echo'
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="index.php" method="post">
+ <p>Introduce la ip: <input type="text" name="nombre" /></p>
+ <p>Su edad: <input type="text" name="edad" /></p>
+ <p><input type="submit" /></p>
+</form>
+</body>
+</html>
+';
 
 require __DIR__ . '/vendor/autoload.php';
 use Foolz\Inet\Inet;
